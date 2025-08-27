39687707056d13d90393e5dbd14bd7ab673d86dd
Pavankumar <114066364+Gopaladas@users.noreply.github.com>
2025-08-28 00:42:37 +0530

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -48,7 +48,7 @@ const signUp = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      maxAge: 7 * 24 * 60 * 60 * 100,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.status(201).json({
